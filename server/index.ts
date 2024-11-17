35e7ec297ab85f8e776a266dfd9a0cab10ab5e47
lari <popoxta@gmail.com>
2024-11-17 20:41:49 +1300

    Fix up routing + add cors

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
 import server from './server.ts'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server listening on port', PORT)
+    // eslint-disable-next-line no-console
+    console.log('Server listening on port', PORT)
 })
+
+
